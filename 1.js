10cef6f6472654457d54a069950d2cb78f5b751c
Valeria Vitvytska <leranmu@gmail.com>
2022-03-27 22:42:29 +0300

    taskcommit

---
+++ 1.js
@@ -0,0 +1,21 @@
+
+'use strict';
+
+
+
+let name = 'Lera';
+let admin = name ;
+console.log(admin);
+
+
+
+let days = 5;
+
+let countSeconds = 86400;
+
+console.log('Result in seconds:',days*countSeconds);
+
+
+const userAge = +prompt('Please,enter your age here!');
+
+console.log(userAge);
